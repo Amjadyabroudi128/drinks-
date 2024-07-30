180903820d53b9a5508b77473a5b52eb1f34c019
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-07-30 01:58:57 +0100

    search controller

--- lib/core/features/home/presentation/Widgets/searchFiled.dart
+++ lib/core/features/home/presentation/Widgets/searchFiled.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Searchfiled extends StatelessWidget {
+class Searchfiled extends StatefulWidget {
   const Searchfiled({super.key});
 
   @override
+  State<Searchfiled> createState() => _SearchfiledState();
+}
+
+class _SearchfiledState extends State<Searchfiled> {
+   TextEditingController searchController = TextEditingController();
+   @override
+   void dispose() {
+     searchController.dispose();
+     super.dispose();
+   }
+  @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return Padding(
       padding: EdgeInsets.all(15.0),
       child: TextField(
-        decoration: InputDecoration(
+        controller: searchController,
+        decoration: const InputDecoration(
           border: OutlineInputBorder(),
           hintText: 'search for drinks',
         ),
