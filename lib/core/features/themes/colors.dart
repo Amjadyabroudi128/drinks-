0c6519c5ebba7b88d792279634827d2517481f18
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-07-31 16:26:02 +0100

    gradient

--- lib/core/features/themes/colors.dart
+++ lib/core/features/themes/colors.dart
@@ -3,3 +3,7 @@ import 'package:flutter/material.dart';
 
 Color blackIcon = Colors.black;
 Color borderColor = Colors.grey;
+ List<Color> searchFiled = [
+  Colors.lightBlueAccent,
+  Colors.white
+];
