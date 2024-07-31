ce6ac443ddb16b6f555cd77b638b0552a269b43f
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-07-31 15:57:08 +0100

    input decoration theme

--- lib/core/features/themes/themes.dart
+++ lib/core/features/themes/themes.dart
@@ -1,7 +1,16 @@
  import 'package:flutter/material.dart';
 
 class AppTheme {
+  static final _border = OutlineInputBorder(
+    borderSide:  BorderSide(color: Colors.grey),
+    borderRadius: BorderRadius.circular(15),
+  );
   static final themeData = ThemeData().copyWith(
-
+    inputDecorationTheme:  InputDecorationTheme(
+      prefixIconColor: Colors.black,
+      suffixIconColor: Colors.black,
+      enabledBorder: _border,
+      focusedBorder: _border,
+    )
   );
  }
