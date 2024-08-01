276db7af937658e1479e890c11d47be7f66f6fad
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-01 12:35:19 +0100

    magnifyingGlass

--- lib/constatns/Constants.dart
+++ lib/constatns/Constants.dart
@@ -1,5 +1,6 @@
 
  import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 const SizedBox sizedBox = SizedBox(height: 6,);
-const Icon search = Icon(Icons.search);
+const Icon search = Icon(FontAwesomeIcons.magnifyingGlass);
