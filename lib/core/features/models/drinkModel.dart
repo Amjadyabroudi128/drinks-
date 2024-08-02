ce7e9a52299477dd9766f2a8f7e33098fc57d8bb
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-02 19:20:55 +0100

    models

---
+++ lib/core/features/models/drinkModel.dart
@@ -0,0 +1,14 @@
+
+
+ class Drinks {
+  late String image;
+  late String name;
+  late int price;
+
+  Drinks({
+    required this.name,
+    required this.image,
+    required this.price,
+ });
+
+ }
