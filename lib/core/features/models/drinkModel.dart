9c1082f879a393d2647d7a23efbb5ce69a2fa3d4
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-02 19:31:02 +0100

    models

--- lib/core/features/models/drinkModel.dart
+++ lib/core/features/models/drinkModel.dart
@@ -1,14 +1,18 @@
 
+ import 'dart:ui';
 
- class Drinks {
+import 'package:flutter/material.dart';
+class Drinks {
   late String image;
   late String name;
-  late int price;
+  late Color color;
 
   Drinks({
     required this.name,
     required this.image,
-    required this.price,
+    required this.color,
  });
-
+  final List<Drinks> drink = [
+    Drinks(name: "Cola", image: "image", color: Colors.red),
+  ];
  }
