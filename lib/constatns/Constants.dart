f6363460de031f00759dcc9cb7705d24c2d5895e
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-02 19:09:17 +0100

    bag

--- lib/constatns/Constants.dart
+++ lib/constatns/Constants.dart
@@ -4,4 +4,4 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 const SizedBox sizedBox = SizedBox(height: 6,);
 const Icon search = Icon(FontAwesomeIcons.magnifyingGlass);
-const Icon bag = Icon(Icons.shopping_bag_outlined, weight: 20, size: 32,);
+const Icon bag = Icon(CupertinoIcons.bag, size: 32, color: Colors.black,);
