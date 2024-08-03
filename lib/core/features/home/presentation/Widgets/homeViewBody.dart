b70cfea5732f69cbb444d5e04a29aa65c1dd1c27
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-03 14:11:57 +0100

    called drinks as drink

--- lib/core/features/home/presentation/Widgets/homeViewBody.dart
+++ lib/core/features/home/presentation/Widgets/homeViewBody.dart
@@ -1,13 +1,15 @@
 import 'package:drinks/core/features/home/presentation/Widgets/searchFiled.dart';
+import 'package:drinks/core/features/models/drinkModel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'CustomAppBar.dart';
 
 class homeViewBody extends StatelessWidget {
-   const homeViewBody({super.key});
-
+   const homeViewBody({super.key, this.drink});
+   final Drinks? drink;
    @override
    Widget build(BuildContext context) {
+
      return GestureDetector(
        onTap: () => FocusScope.of(context).requestFocus( FocusNode()),
        child: const Scaffold(
