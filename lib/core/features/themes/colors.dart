2765d1178f4cb23f4c55cadfee48ab3cdf30334b
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-04 15:34:33 +0100

    hint color

--- lib/core/features/themes/colors.dart
+++ lib/core/features/themes/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 Color blackIcon = Colors.black;
 Color borderColor = Colors.black;
+Color hintColor = Colors.grey;
  List<Color> searchFiled = [
   Colors.cyan[200]!,
   Colors.white
