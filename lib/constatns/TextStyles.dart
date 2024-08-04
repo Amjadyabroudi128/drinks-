d339b0fe929fa9c4e1f910e6e6fe0d337d0857e6
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-04 15:34:49 +0100

    bold textstyle

--- lib/constatns/TextStyles.dart
+++ lib/constatns/TextStyles.dart
@@ -1,10 +1,13 @@
-
+import 'package:drinks/core/features/themes/colors.dart';
 import 'package:flutter/material.dart';
  class TextStyles {
-   static TextStyle hint = const TextStyle(
-       color: Colors.grey
+   static TextStyle hint = TextStyle(
+       color: hintColor
    );
    static TextStyle kFont19 = const TextStyle(
        fontSize: 19.0, color: Colors.black
+   );
+   static TextStyle Bold19 = const TextStyle(
+       fontSize: 19.0, color: Colors.black, fontWeight: FontWeight.bold
    );
  }
