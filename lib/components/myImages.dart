f47665803c8e641331628cb657bc3a56645191ac
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-05 17:38:58 +0100

    images class

--- lib/components/myImages.dart
+++ lib/components/myImages.dart
@@ -1,3 +1,7 @@
  class myImages {
   String cola = "images/cola.png";
+  String dew = "images/dew.png";
+  String fanta = "images/fanta.png";
+  String pepsi = "images/pepsi.png";
+  String sprite = "images/sprite.png";
  }
