988fb5f6b7768f399fe73a20c80add821054aba6
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-06 14:36:32 +0100

    Drink list

--- lib/core/features/home/presentation/Widgets/homeViewBody.dart
+++ lib/core/features/home/presentation/Widgets/homeViewBody.dart
@@ -19,7 +19,7 @@ class homeViewBody extends StatelessWidget {
            children: [
              CustomAppBar(),
              Searchfiled(),
-             DrinksList(),
+             DrinksList()
            ],
          ),
        ),
