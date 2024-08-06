864fc07f0d53390642a196c5bafbd8db883b22c8
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-06 19:06:08 +0100

    drinks list

--- lib/constatns/drinksList.dart
+++ lib/constatns/drinksList.dart
@@ -4,7 +4,7 @@ import '../components/myImages.dart';
 import '../core/features/models/drinkModel.dart';
 
 final List<Drinks> drinks = [
-  Drinks(name: "Cola", image: myImages.cola, color: Colors.red),
+  Drinks(name: "cola", image: myImages.cola, color: Colors.red),
   Drinks(name: "dew", image: myImages.dew, color: Colors.greenAccent),
   Drinks(name: "fanta", image: myImages.fanta, color: Colors.orange),
   Drinks(name: "pepsi", image: myImages.pepsi, color: Colors.blue),
