a2681196b0877501de97ff406916eefd87003502
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-09 16:06:12 +0100

    translate widget

---
+++ lib/core/features/home/presentation/Widgets/translateWidget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../../../models/drinkModel.dart';
+import 'CupPainter.dart';
+
+class Translate extends StatelessWidget {
+  const Translate({
+    super.key,
+    required this.drink,
+  });
+
+  final Drinks drink;
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(0, -8),
+      child: Center(
+        child: CustomPaint(
+          painter: CupPainter(drink: drink),
+          child: Container(
+            width: 120,
+            height: 150,
+            alignment: Alignment.center,
+            child: Image.asset(drink.image, width: 50, height: 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
