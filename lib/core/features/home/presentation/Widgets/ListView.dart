2768ae30a7befdfef65906b6d8dd3c5785b141ca
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-10 16:31:45 +0100

    only wrap the card with gesture detector

--- lib/core/features/home/presentation/Widgets/ListView.dart
+++ lib/core/features/home/presentation/Widgets/ListView.dart
@@ -1,39 +1,48 @@
 import 'package:drinks/constatns/drinksList.dart';
-import 'package:drinks/core/features/details/presentation/detailsPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
 
+import '../../../details/presentation/detailsPage.dart';
 import '../../../models/drinkModel.dart';
 import 'DrinkCard.dart';
-
 class DrinksList extends StatelessWidget {
   final Drinks? drink;
 
   const DrinksList({super.key, this.drink,});
+
   @override
   Widget build(BuildContext context) {
-    return  GestureDetector(
-      onTap: (){
-      },
-      child: ScrollConfiguration(
-        behavior: ScrollBehavior(),
-        child: SingleChildScrollView(
-          child: GridView.builder(
-            shrinkWrap: true,
-            itemCount: drinks.length,
-            itemBuilder: (context, index) {
-              return Transform.translate(
-                  offset: Offset(0, index.isEven ? -28 : -5), // Adjust the offset based on the index
-                  child: DrinkCard(drink: drinks[index])
-              );
-            }, gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, mainAxisExtent: 200,
-          ),
+    return ScrollConfiguration(
+      behavior: ScrollBehavior(),
+      child: SingleChildScrollView(
+        child: GridView.builder(
+          shrinkWrap: true,
+          itemCount: drinks.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Detailspage(drink: drinks[index]),
+                  ),
+                );
+              },
+              child: Transform.translate(
+                offset: Offset(0, index.isEven ? -28 : -5),
+                child: DrinkCard(drink: drinks[index]),
+              ),
+            );
+          },
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisExtent: 200,
           ),
         ),
       ),
     );
   }
 }
+
