c359af84ee31c7ab8aabb1055fc632af557bb42c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-10 16:31:59 +0100

    details

--- lib/core/features/details/presentation/detailsPage.dart
+++ lib/core/features/details/presentation/detailsPage.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
+import '../../models/drinkModel.dart';
+
 class Detailspage extends StatelessWidget {
   static const String id = "detailsPage";
+  final Drinks drink;
 
-  const Detailspage({super.key});
+  const Detailspage({super.key, required this.drink});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Hello Wrold", style: TextStyle(
+        child: Text(drink.name, style: TextStyle(
           fontWeight: FontWeight.bold,
           fontSize: 50
         ),),
