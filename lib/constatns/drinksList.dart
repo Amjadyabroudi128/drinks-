cbe12e770b5bc231042c34feacf099ab77e719d6
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-11 14:58:14 +0100

    orange color

--- lib/constatns/drinksList.dart
+++ lib/constatns/drinksList.dart
@@ -6,7 +6,7 @@ import '../core/features/models/drinkModel.dart';
 final List<Drinks> drinks = [
   Drinks(name: "cola", image: myImages.cola, color: Colors.redAccent.shade200, cupColor: Colors.red.shade800),
   Drinks(name: "dew", image: myImages.dew, color: Colors.lightGreenAccent, cupColor: Colors.lightGreenAccent),
-  Drinks(name: "fanta", image: myImages.fanta, color: Colors.orange, cupColor: Colors.orange),
+  Drinks(name: "fanta", image: myImages.fanta, color: Colors.orange.shade300, cupColor: Colors.orange.shade900),
   Drinks(name: "pepsi", image: myImages.pepsi, color: Colors.blue.shade300, cupColor: Colors.blue.shade900),
   Drinks(name: "sprite", image: myImages.sprite, color: Colors.green, cupColor: Colors.green),
   Drinks(name: "pepper", image: myImages.pepper, color: Colors.red.shade800, cupColor: Colors.red.shade700),
