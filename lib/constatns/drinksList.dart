5db5df4a83edc35b05ad084b766ef4528e159230
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-11 14:37:03 +0100

    cup Color

--- lib/constatns/drinksList.dart
+++ lib/constatns/drinksList.dart
@@ -4,10 +4,10 @@ import '../components/myImages.dart';
 import '../core/features/models/drinkModel.dart';
 
 final List<Drinks> drinks = [
-  Drinks(name: "cola", image: myImages.cola, color: Colors.red),
-  Drinks(name: "dew", image: myImages.dew, color: Colors.lightGreenAccent),
-  Drinks(name: "fanta", image: myImages.fanta, color: Colors.orange),
-  Drinks(name: "pepsi", image: myImages.pepsi, color: Colors.blue),
-  Drinks(name: "sprite", image: myImages.sprite, color: Colors.green),
-  Drinks(name: "pepper", image: myImages.pepper, color: Colors.red.shade800),
+  Drinks(name: "cola", image: myImages.cola, color: Colors.red, cupColor: Colors.red),
+  Drinks(name: "dew", image: myImages.dew, color: Colors.lightGreenAccent, cupColor: Colors.lightGreenAccent),
+  Drinks(name: "fanta", image: myImages.fanta, color: Colors.orange, cupColor: Colors.orange),
+  Drinks(name: "pepsi", image: myImages.pepsi, color: Colors.blue, cupColor: Colors.blue),
+  Drinks(name: "sprite", image: myImages.sprite, color: Colors.green, cupColor: Colors.green),
+  Drinks(name: "pepper", image: myImages.pepper, color: Colors.red.shade800, cupColor: Colors.red.shade700),
 ];
