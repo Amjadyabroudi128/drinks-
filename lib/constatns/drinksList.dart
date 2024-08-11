5a45db85c6a4daa51393ecef549b3cb6cccb4eb8
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-11 15:21:15 +0100

    pepper color

--- lib/constatns/drinksList.dart
+++ lib/constatns/drinksList.dart
@@ -8,6 +8,6 @@ final List<Drinks> drinks = [
   Drinks(name: "dew", image: myImages.dew, color: Colors.lightGreenAccent, cupColor: Colors.lightGreenAccent),
   Drinks(name: "fanta", image: myImages.fanta, color: Colors.orange.shade300, cupColor: Colors.orange.shade900),
   Drinks(name: "pepsi", image: myImages.pepsi, color: Colors.blue.shade300, cupColor: Colors.blue.shade900),
-  Drinks(name: "sprite", image: myImages.sprite, color: Colors.green, cupColor: Colors.green),
-  Drinks(name: "pepper", image: myImages.pepper, color: Colors.red.shade800, cupColor: Colors.red.shade700),
+  Drinks(name: "sprite", image: myImages.sprite, color: Colors.green.shade300, cupColor: Colors.green),
+  Drinks(name: "pepper", image: myImages.pepper, color: Colors.red.shade200, cupColor: Colors.brown.shade800),
 ];
