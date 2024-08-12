0ae073adc83eee992811342aed7d0372ba1fc037
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-12 14:04:41 +0100

    cup

---
+++ lib/core/features/details/presentation/widgets/cup.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../../home/presentation/Widgets/CupPainter.dart';
+import '../../../models/drinkModel.dart';
+
+class CupPlace extends StatelessWidget {
+  const CupPlace({
+    super.key,
+    required this.drink,
+  });
+
+  final Drinks drink;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 68), // Padding above the image remains
+      child: Stack(
+        children: [
+          Center(
+            child: CustomPaint(
+              painter: CupPainter(drink: drink),
+              child: Container(
+                width: 120,
+                height: 180, // Adjust this if needed
+                alignment: Alignment.center,
+                child: Image.asset(drink.image, width: 69, height: 63),
+              ),
+            ),
+          ),
+          const Positioned(
+            height: 22,
+            bottom: -9, // Aligns the Divider at the bottom of the Stack
+            left: 0,
+            right: 0,
+            child: Divider(
+              color: Colors.black45,
+              thickness: 4,
+              indent: 169,
+              endIndent: 140,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
