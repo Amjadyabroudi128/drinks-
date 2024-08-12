e6e4e7047b8255aa5f4c7df962a2625b79be9986
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-12 14:05:13 +0100

    changed dew color

--- lib/constatns/drinksList.dart
+++ lib/constatns/drinksList.dart
@@ -5,7 +5,7 @@ import '../core/features/models/drinkModel.dart';
 
 final List<Drinks> drinks = [
   Drinks(name: "cola", image: myImages.cola, color: Colors.redAccent.shade200, cupColor: Colors.red.shade800),
-  Drinks(name: "dew", image: myImages.dew, color: Colors.lightGreenAccent, cupColor: Colors.lightGreenAccent),
+  Drinks(name: "dew", image: myImages.dew, color: Colors.lightGreenAccent, cupColor: Colors.lightGreen.shade200),
   Drinks(name: "fanta", image: myImages.fanta, color: Colors.orange.shade300, cupColor: Colors.orange.shade900),
   Drinks(name: "pepsi", image: myImages.pepsi, color: Colors.blue.shade300, cupColor: Colors.blue.shade900),
   Drinks(name: "sprite", image: myImages.sprite, color: Colors.green.shade300, cupColor: Colors.green),
