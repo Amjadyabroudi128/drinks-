c6501a9dee1e689c235cfcfc6b2a0eefef1cceb1
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-12 13:15:02 +0100

    app bar details

---
+++ lib/core/features/details/presentation/widgets/detailsBar.dart
@@ -0,0 +1,29 @@
+import 'package:drinks/core/features/details/presentation/detailsPage.dart';
+import 'package:drinks/core/features/home/presentation/Widgets/homeViewBody.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../components/sizedBox.dart';
+import '../../../../../constatns/Constants.dart';
+import '../../../models/drinkModel.dart';
+
+class detailsBar extends StatelessWidget {
+  const detailsBar({
+    super.key,
+    required this.drink,
+  });
+
+  final Drinks drink;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(icon: goBack, onPressed: (){
+          Navigator.of(context).pushNamed(homeViewBody.id);
+        },),
+        sizedBox(width: 3,),
+        Text(drink.name, style: TextStyle(color: Colors.white, fontSize: 30),),
+      ],
+    );
+  }
+}
