9726477cf8eea7f3ba5a8bc459ed4f0fe7991130
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-12 01:31:01 +0100

    details page

--- lib/core/features/details/presentation/detailsPage.dart
+++ lib/core/features/details/presentation/detailsPage.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 import '../../models/drinkModel.dart';
@@ -10,13 +11,28 @@ class Detailspage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(drink.name, style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 50
-        ),),
-      ),
-    );
+     return Scaffold(
+       body: Padding(
+        padding: const EdgeInsets.only(left: 10,top: 50),
+        child: Container(
+          decoration: BoxDecoration(
+
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  IconButton(icon: Icon(Icons.arrow_back) , onPressed: (){
+                    Navigator.of(context).pop();
+                  },),
+                  SizedBox(width: 20,),
+                  Text(drink.name, style: TextStyle(fontSize: 40),)
+                ],
+              ),
+            ],
+          ),
+        ),
+           ),
+     );
   }
 }
