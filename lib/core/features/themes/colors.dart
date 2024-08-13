190508d21a8d2bbee4146f64f2137b3a749b50cd
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-13 15:36:09 +0100

    default icon Color

--- lib/core/features/themes/colors.dart
+++ lib/core/features/themes/colors.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 Color blackIcon = Colors.black;
 Color borderColor = Colors.black;
 Color hintColor = Colors.grey;
+Color defaultIconColor = Colors.white;
  List<Color> searchFiled = [
   Colors.cyan[200]!,
   Colors.white
