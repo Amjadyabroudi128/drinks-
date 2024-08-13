b97ec9bb612f5e491cce8ee9a5d0e02aee804bad
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-13 15:36:32 +0100

    default icon size

--- lib/constatns/Constants.dart
+++ lib/constatns/Constants.dart
@@ -1,5 +1,5 @@
 
- import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 const Icon search = Icon(FontAwesomeIcons.magnifyingGlass);
@@ -7,3 +7,4 @@ const Icon bag = Icon(CupertinoIcons.bag, size: 32, color: Colors.black,);
 const Icon goBack = Icon(Icons.arrow_back,);
 const Icon remove = Icon(Icons.remove_circle_outline,);
 const Icon add = Icon(Icons.add_circle_outline,);
+const double defaultIconSize = 30;
