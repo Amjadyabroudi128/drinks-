dde04228687388e0b33f8765d1ad998cb8551ee0
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-14 19:46:00 +0100

    icon size

--- lib/components/IconButton.dart
+++ lib/components/IconButton.dart
@@ -4,12 +4,13 @@ class MyIcons extends StatelessWidget {
   final Icon? icon;
   final VoidCallback? onPressed;
   final Color? clr;
-
-  const MyIcons({super.key, this.clr, this.onPressed, this.icon});
+  final double? size;
+  const MyIcons({super.key, this.clr, this.onPressed, this.icon, this.size});
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
+      iconSize: size ,
       onPressed: onPressed,
       icon: icon ?? Icon(Icons.error), // Provide a default icon to handle null
       color: clr,
