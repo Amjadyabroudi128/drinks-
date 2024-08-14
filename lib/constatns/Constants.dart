fef6a2262daf98c1d4bf01f042850b5967c3beb5
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-14 19:45:29 +0100

    default color and size

--- lib/constatns/Constants.dart
+++ lib/constatns/Constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 const Icon search = Icon(FontAwesomeIcons.magnifyingGlass);
-const Icon bag = Icon(CupertinoIcons.bag, size: 32, color: Colors.black,);
+const Icon bag = Icon(CupertinoIcons.bag,);
 const Icon goBack = Icon(Icons.arrow_back,);
 const Icon remove = Icon(Icons.remove_circle_outline,);
 const Icon add = Icon(Icons.add_circle_outline,);
