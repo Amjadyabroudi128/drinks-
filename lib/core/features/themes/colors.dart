a47e5486aa3c1b7411ee08fd20ba4224dc2568b6
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-16 12:54:13 +0100

    unselected color

--- lib/core/features/themes/colors.dart
+++ lib/core/features/themes/colors.dart
@@ -5,6 +5,7 @@ Color blackIcon = Colors.black;
 Color borderColor = Colors.black;
 Color hintColor = Colors.grey;
 Color defaultIconColor = Colors.white;
+Color unSelected = Colors.grey;
  List<Color> searchFiled = [
   Colors.cyan[200]!,
   Colors.white
