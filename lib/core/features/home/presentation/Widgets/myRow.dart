8b143dd3bd80d762b1a10baa645232391978afc7
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-19 14:23:49 +0100

    extracted Widgets

--- lib/core/features/home/presentation/Widgets/myRow.dart
+++ lib/core/features/home/presentation/Widgets/myRow.dart
@@ -13,20 +13,42 @@ class myRow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Card(
-          shape: const CircleBorder(),
-          clipBehavior: Clip.antiAlias,
-          child: Image.asset(myImages.amjad, height: 58,),
-        ),
+        CardImage(),
         Spacer(),
-        MyIcons(
-          icon: bag,
-          size: 42,
-          clr: Colors.black,
-          onPressed: (){
-          },
-        )
+        BagIcon()
       ],
+    );
+  }
+}
+
+class CardImage extends StatelessWidget {
+  const CardImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: const CircleBorder(),
+      clipBehavior: Clip.antiAlias,
+      child: Image.asset(myImages.amjad, height: 58,),
+    );
+  }
+}
+
+class BagIcon extends StatelessWidget {
+  const BagIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MyIcons(
+      icon: bag,
+      size: 42,
+      clr: Colors.black,
+      onPressed: (){
+      },
     );
   }
 }
