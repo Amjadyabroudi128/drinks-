5886fba1513368100d62294ea5b2aa070f3e802d
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-21 15:54:23 +0100

    width and height

--- lib/components/myContainer.dart
+++ lib/components/myContainer.dart
@@ -5,13 +5,17 @@ class myContainer extends StatelessWidget {
   final BoxBorder? border;
   final Widget? child;
   final AlignmentGeometry? alignment;
-  const myContainer({super.key,  this.decoration, this.border, this.child, this.alignment});
+  final double? height;
+  final double? width;
+  const myContainer({super.key,  this.decoration, this.border, this.child, this.alignment, this.height, this.width});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: decoration,
       alignment: alignment,
+      height: height,
+      width: width,
       child: child,
     );
   }
