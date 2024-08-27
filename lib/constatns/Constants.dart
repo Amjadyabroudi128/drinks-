947537b219f0bb26a91dc630615e27430b1079d4
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-27 12:35:12 +0100

    separating consts

--- lib/constatns/Constants.dart
+++ lib/constatns/Constants.dart
@@ -2,17 +2,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// icons
 const Icon search = Icon(FontAwesomeIcons.magnifyingGlass);
 const Icon bag = Icon(CupertinoIcons.bag,);
 const Icon goBack = Icon(Icons.arrow_back,);
 const Icon remove = Icon(Icons.remove_circle_outline,);
 const Icon add = Icon(Icons.add_circle_outline,);
+ // measurements
 const double defaultIconSize = 30;
-const FontWeight normal = FontWeight.normal;
-const FontWeight bold = FontWeight.bold;
 const double imageWidth = 32;
 const double imageHeight = 63;
 const double cardHeight = 155;
 const double cardWidth = 163;
 const double radius = 12;
+// fonts
+const FontWeight normal = FontWeight.normal;
+const FontWeight bold = FontWeight.bold;
 
