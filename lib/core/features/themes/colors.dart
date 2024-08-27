d4deacb72b384b6b82a5b9b0d6eb909b2484eefd
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2024-08-27 12:41:51 +0100

    colors ml

--- lib/core/features/themes/colors.dart
+++ lib/core/features/themes/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 Color blackIcon = Colors.black;
 Color borderColor = Colors.black;
+Color mlClr = Colors.black45;
 Color hintColor = Colors.grey;
 Color defaultIconColor = Colors.white;
 Color unSelected = Colors.grey;
